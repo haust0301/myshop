0d190527067397e5f04d0b6554397e4be63aef1f
haust0301 <phanthanhhaust@gmail.com>
2022-09-18 13:58:40 +0700

    Khoi tao du an

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple),
+      ).copyWith(
+        secondaryHeaderColor: Colors.deepOrange,
+      ),
+      home: Container(
+        color: Colors.green,
+      ),
+    );
+  }
+}
